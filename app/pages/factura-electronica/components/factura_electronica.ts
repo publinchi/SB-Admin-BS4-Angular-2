0a098a3f510fb8f3af2835eb3ddaa91f6ce00a8b
publinchi <publinchi@gmail.com>
2016-07-07 22:59:33 -0500

    Se crea componente FacturaElectronicaComponent

---
+++ app/pages/factura-electronica/components/factura_electronica.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'factura-electronica',
+	templateUrl: 'factura-electronica.html'
+})
+
+export class FacturaElectronicaComponent {}
